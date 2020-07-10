9d2cdc6c58576aa301165d144c090e4cf82d1019
Ega Pamungkas Susanto <45747765+B0urG3ois@users.noreply.github.com>
2020-07-10 19:42:56 +0700

    Mean Vectorizer FastText

---
+++ MeanVectorizerFT.py
@@ -0,0 +1,20 @@
+import numpy as np
+import logging
+
+class MeanEmbeddingVectorizer(object):
+    def __init__(self, word2vec):
+        self.word2vec = word2vec
+        # if a text is empty we should return a vector of zeros
+        # with the same dimensionality as all the other vectors
+        # self.dim = word2vec.wv.vector_size
+        self.dim = word2vec.get_dimension()
+
+    def fit(self, X, y):
+        return self
+
+    def transform(self, X):
+        return np.array([
+            np.mean([self.word2vec[w] for w in words if w in self.word2vec]
+                    or [np.zeros(self.dim)], axis=0)
+            for words in X
+        ])
